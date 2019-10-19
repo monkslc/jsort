bc06ebc6ff6c7b4203c90be2f56e0d2cd4279b1f
Connor Monks <connor-monks@ip-10-0-0-183.us-west-2.compute.internal>
2019-10-19 06:42:10 -0400

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use serde_json::Value;
 use std::collections::HashMap;
+use std::io::{self, Read};
 
 fn main() {
-	
-	let data = r#"{ "name": "Jon", "age": 37 }"#;	
-	let v: HashMap<String, Value> = serde_json::from_str(data).expect("Couldn't deserialize here");
-		
-    println!("Name: {}, Age: {}", v["name"], v["age"]);
+    let mut data = String::new();
+
+    io::stdin()
+        .read_to_string(&mut data)
+        .expect("Couldn't read from stdin");
+
+    let json: HashMap<String, Value> =
+        serde_json::from_str(&data[..]).expect("Couldn't deserialize here");
+
+    let mut keys = Vec::new();
+    for key in json.keys() {
+        keys.push(key)
+    }
+
+    keys.sort();
+
+    println!("{{");
+    for key in keys {
+        // In here we add a check if the json value is a hash map
+        // If so, we need to loop through those keys and do the same process
+        println!("\t{}: {}", key, json[key]);
+    }
+    println!("}}");
 }
